8ef69af4d7cce85b395e41529709101b7300c2e7
Eliez MAIGNÉ <eliez_lp@hotmail.com>
2017-10-22 12:27:38 +0200

    Add function SymbolChain
    Function SymbolChain now display 5 Katakanas instead of just one.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 var symbol;
 var symbolSize = 45;
+var chain;
 
 function setup() {
     createCanvas(
@@ -17,12 +18,15 @@ function setup() {
         5
     );
     symbol.setToRandomSymbol();
+    chain = new SymbolChain();
+    chain.generateSymbols();
     textSize(symbolSize);
 }
 
 function draw() {
     background(0);
     symbol.render();
+    chain.render();
 }
 
 function Symbol(x, y) {
@@ -60,5 +64,29 @@ function Symbol(x, y, speed) {
         }
         
         this.y += this.speed;
+    }
+}
+function SymbolChain() {
+    this.symbols = [];
+    this.totalSymbols = 5;
+    this.speed = 5;
+    this.generateSymbols = function () {
+        var y = 0;
+        var x = width / 2;
+        for (var i = 0; i <= this.totalSymbols; i++) {
+            symbol = new Symbol(x, y, this.speed);
+            symbol.setToRandomSymbol();
+            this.symbols.push(symbol);
+            y -= symbolSize;
+        }
+
+    }
+    this.render = function () {
+        this.symbols.forEach(function (symbol) {
+            fill(0, 255, 70);
+            text(symbol.value, symbol.x, symbol.y);
+            symbol.fall();
+            symbol.setToRandomSymbol();
+        });
     }
 }
